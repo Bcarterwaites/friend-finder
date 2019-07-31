7031215ab17712b7889ac7b6b15467b9b6ab27a9
Bcarterwaites <44250144+Bcarterwaites@users.noreply.github.com>
2019-07-31 07:39:00 -0400

    Add files via upload

---
+++ app/data/friends.js
@@ -0,0 +1,98 @@
+var friends = [
+    {
+    name: "Steven",
+    photo: "https://en.wikipedia.org/wiki/Steven_Universe_(character)#/media/File:Steven_Universe.png",
+    scores: [
+        "2",
+        "3",
+        "1",
+        "4",
+        "4",
+        "1",
+        "1",
+        "3",
+        "4",
+        "2"
+        
+
+    ]
+},
+{
+    name: "Craig",
+    photo: "https://vignette.wikia.nocookie.net/scratchpad/images/b/b4/Craig_%28Craig_of_the_Creek%29.png/revision/latest?cb=20180128011801",
+    scores: [
+        "1",
+        "4",
+        "3",
+        "2",
+        "2",
+        "3",
+        "1",
+        "4",
+        "2",
+        "3"
+        
+
+
+    ]
+},
+{
+    name: "Finn",
+    photo: "https://en.wikipedia.org/wiki/Finn_the_Human#/media/File:FinnAdventureTime.png",
+    scores: [
+        "3",
+        "4",
+        "1",
+        "2",
+        "2",
+        "1",
+        "3",
+        "4",
+        "1",
+        "2"
+        
+
+
+    ],
+
+},
+{
+    name: "Margret",
+    photo: "https://regularshow.fandom.com/wiki/Margaret?file=Reg_margaret.png",
+    scores: [
+        "4",
+        "4",
+        "1",
+        "2",
+        "3",
+        "2",
+        "3",
+        "1",
+        "1",
+        "2"
+
+    ]
+},
+
+{
+    name: "Marceline",
+    photo: "https://www.discogs.com/artist/4781237-Marceline-The-Vampire-Queen#images/30939586",
+    scores: [
+        
+        "2",
+        "2",
+        "4",
+        "3",
+        "1",
+        "1",
+        "3",
+        "4",
+        "4",
+        "2"
+
+
+    ]
+
+}]
+
+module.exports = friends;
